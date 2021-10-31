de3e7665bf4836492bc19170b51239bdbfe3a4b8
zhetiew <zhetiew@gmail.com>
2021-10-31 07:45:17 +0000

    add pages again

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,32 @@
 <script>
-	import '../app.scss';
+    import '../app.scss'; // Bootstrap 5
+    import supabase from '$lib/db';
+    import { page, session } from '$app/stores';
+    import { browser } from '$app/env';
+    import { goto } from '$app/navigation';
+
+    // code that runs only in the browser
+    if (browser) {
+   	 $session = supabase.auth.session(); // set session
+   	 redirect();
+
+   	 supabase.auth.onAuthStateChange((userSession) => {
+   		 $session = userSession; // set session
+   		 redirect();
+   	 });
+    }
+
+    function redirect() {
+   	 //login redirect
+   	 if ($session && $page.path === '/') {
+   		 goto('/welcome');
+   	 }
+
+   	 //logout redirect
+   	 if (!$session && $page.path === '/welcome') {
+   		 goto('/');
+   	 }
+    }
 </script>
 
 <slot />
---
+++ src/routes/welcome.svelte
@@ -0,0 +1,15 @@
+<script>
+    import supabase from '$lib/db';
+
+    async function signOut() {
+   	 const { error } = await supabase.auth.signOut();
+
+   	 if (error) alert(error.message); // alert if error
+    }
+</script>
+
+<div class="text-center">
+    <h1 class="bg-warning p-5">Welcome!</h1>
+
+    <button on:click={signOut}>Logout</button>
+</div>
