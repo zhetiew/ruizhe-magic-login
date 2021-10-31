757423c5bdf4d719d38cab7ba5549767265a982f
zhetiew <zhetiew@gmail.com>
2021-10-31 07:44:12 +0000

    added config page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import supabase from '$lib/db';
+
+    let email;
+
+    async function loginUser() {
+   	 const { error } = await supabase.auth.signIn({
+   		 email
+   	 });
+
+   	 if (error) alert(error.message); // alert if error
+    }
+</script>
+
+<h1 class="bg-primary text-white p-5 text-center">Homepage</h1>
+
+<div class="text-center">
+    <form on:submit|preventDefault={loginUser}>
+   	 <label for="email">Email</label>
+   	 <input id="email" bind:value={email} type="email" />
+
+   	 <button>Magic</button>
+    </form>
+</div>
